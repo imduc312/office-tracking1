f478899e69ad8ee8a53b756976fba4f3f3264e14
CongDuc312 <trancongduc312201@gmail.com>
2023-10-10 11:49:32 +0700

    comic1010

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,18 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CommonModule } from '@angular/common'; // Import CommonModule
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    CommonModule,
+    FormsModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,106 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonModal) modal!: IonModal;
 
+  name!: string;
+  selectedData: { [key: string]: string } = {};
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    const dataFromSelectConfim = this.selectedData;
+    console.log(dataFromSelectConfim);
+  }
+  selectStatus(selected: any) {
+    this.selectedData = selected;
+  }
   constructor() {}
 
+  // onclick
+  isOpen: boolean = false;
+  onClickOpenModal(
+    isOpen: boolean,
+    data?: {
+      dayNumber: string;
+      dayText: string;
+      morning: string;
+      afternoon: string;
+      evening: string;
+    }
+  ) {
+    this.isOpen = isOpen;
+    if (data) {
+      const daily = this.dailies.find((d) =>
+        d.schedules.some((s) => s === data)
+      );
+      if (daily) {
+        daily.selectedStatus = ''; // Đặt lại trạng thái khi mở modal
+      }
+    }
+  }
+  dailies = [
+    {
+      user: {
+        avatar:
+          'https://lh3.googleusercontent.com/a/ACg8ocIhAMTa1Zfo_9EXA67v0ICHaChKnpltNnHzj2d1JznyQuE=s360-c-no',
+        name: 'Le Van A',
+      },
+      schedules: [
+        {
+          dayNumber: '02',
+          dayText: 'Mon',
+          morning: 'Office',
+          afternoon: 'Office',
+          evening: 'Offline',
+        },
+        {
+          dayNumber: '03',
+          dayText: 'Tus',
+          morning: 'Office',
+          afternoon: 'Office',
+          evening: 'Offline',
+        },
+        {
+          dayNumber: '04',
+          dayText: 'Wed',
+          morning: 'Office',
+          afternoon: 'Office',
+          evening: 'Offline',
+        },
+        {
+          dayNumber: '05',
+          dayText: 'Thu',
+          morning: 'Office',
+          afternoon: 'Office',
+          evening: 'Offline',
+        },
+        {
+          dayNumber: '06',
+          dayText: 'Fri',
+          morning: 'Office',
+          afternoon: 'Office',
+          evening: 'Online',
+        },
+        {
+          dayNumber: '07',
+          dayText: 'Sat',
+          morning: 'Online',
+          afternoon: 'Online',
+          evening: 'Offline',
+        },
+
+        {
+          dayNumber: '08',
+          dayText: 'Sun',
+          morning: 'Online',
+          afternoon: 'Offline',
+          evening: 'Offline',
+        },
+      ],
+      selectedStatus: '', // Thêm trạng thái được chọn cho mỗi phần tử
+    },
+  ];
 }
+// test log
